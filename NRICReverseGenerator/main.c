849d668a80aaaa07bfaaf0e5c0ac5c22c3f9ef2d
Guan Kiat <lauguankiat@gmail.com>
2025-09-18 13:56:38 +0800

    Initial Commit

---
+++ NRICReverseGenerator/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  NRICReverseGenerator
+//
+//  Created by GK on 18/9/25.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
