47644f47b07350f2677840e31ba0a681a70399ac
Guan Kiat <lauguankiat@gmail.com>
2025-09-22 13:49:57 +0800

    complete digit4 reference table

--- NRICReverseGenerator/digit4.h
+++ NRICReverseGenerator/digit4.h
@@ -124,6 +124,121 @@ void setDigit4(void) {
             default:
                 // Handle invalid input or errors here.
                 break;
-}
+        }
+    }
+    
+    if (digit3 == 3) {
+        switch (alpha8) {
+            case 'G':
+                digit4 = 0;
+                break;
+            case 'B':
+                digit4 = 1;
+                break;
+            case 'H':
+                digit4 = 2;
+                break;
+            case 'C':
+                digit4 = 3;
+                break;
+            case 'I':
+                digit4 = 4;
+                break;
+            case 'D':
+                digit4 = 5;
+                break;
+            case 'Z':
+                digit4 = 6;
+                break;
+            case 'E':
+                digit4 = 7;
+                break;
+            case 'J':
+                digit4 = 8;
+                break;
+            case 'F':
+                digit4 = 9;
+                break;
+            default:
+                // Handle invalid input or errors here.
+                break;
+        }
+    }
+    
+    if (digit3 == 4) {
+        switch (alpha8) {
+            case 'A':
+                digit4 = 0;
+                break;
+            case 'G':
+                digit4 = 1;
+                break;
+            case 'B':
+                digit4 = 2;
+                break;
+            case 'H':
+                digit4 = 3;
+                break;
+            case 'C':
+                digit4 = 4;
+                break;
+            case 'I':
+                digit4 = 5;
+                break;
+            case 'D':
+                digit4 = 6;
+                break;
+            case 'Z':
+                digit4 = 7;
+                break;
+            case 'E':
+                digit4 = 8;
+                break;
+            case 'J':
+                digit4 = 9;
+                break;
+            default:
+                // Handle invalid input or errors here.
+                break;
+        }
+    }
 
+    if (digit3 == 5) {
+        switch (alpha8) {
+            case 'F':
+                digit4 = 0;
+                break;
+            case 'A':
+                digit4 = 1;
+                break;
+            case 'G':
+                digit4 = 2;
+                break;
+            case 'B':
+                digit4 = 3;
+                break;
+            case 'H':
+                digit4 = 4;
+                break;
+            case 'C':
+                digit4 = 5;
+                break;
+            case 'I':
+                digit4 = 6;
+                break;
+            case 'D':
+                digit4 = 7;
+                break;
+            case 'Z':
+                digit4 = 8;
+                break;
+            case 'E':
+                digit4 = 9;
+                break;
+            default:
+                // Handle invalid input or errors here.
+                break;
+        }
+    }
+    
 #endif
